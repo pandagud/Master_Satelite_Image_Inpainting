8ddf19dbf80d84735987452e65d206b5aa638559
pandagud <pandagud@gmail.com>
2020-10-08 11:05:03 +0200

    Change to dataloader. Multiban

--- src/dataLayer/importRGB.py
+++ src/dataLayer/importRGB.py
@@ -4,14 +4,43 @@ import os
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torch
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import numpy as np
+
+
+class SatelliteDataset(Dataset):
+    def __init__(self, data, transform=None):
+        self.data = torch.from_numpy(data).float()
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x = self.data[index]
+
+        if self.transform:
+            x = self.transform(x)
+
+        return x
+
+    def __len__(self):
+        return len(self.data)
 class importData():
     def __init__(self):
         self.localdir = pathlib.Path().absolute().parent
         self.processed_path = Path.joinpath(self.localdir, 'data\\processed')
 
-
-
     def getRBGDataLoader(self,config):
+
+        raw_rgb =self.get_images_array(invert=True)
+        localtransform = transforms.Compose([
+            transforms.Resize(config.image_size),
+            transforms.CenterCrop(config.image_size),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+        raw_rgbData = SatelliteDataset(raw_rgb,localtransform)
+        test_data_loader = torch.utils.data.DataLoader(raw_rgbData, batch_size=config.batch_size,
+                                                       shuffle=False, num_workers=config.workers)
         ## implemented to look into all for folders located in processed
         subfolders = [f.path for f in os.scandir(self.processed_path) if f.is_dir()]
         for folder in subfolders:
@@ -31,6 +60,33 @@ class importData():
                                            shuffle=False, num_workers=config.workers)
         # Create the dataloader
         return train_data_loader,test_data_loader
+    def open_files(self, path):
+        import glob
+        path_images = Path.joinpath(self.processed_path, path)
+        str_path = str(path_images)
+        filelist = glob.glob(str_path+'/*.tiff')
+        x = np.array([np.array(Image.open(fname)) for fname in filelist])
+        return x
+
+    def get_images_array(self, invert=False, include_nir=False):
+        red_images = self.open_files('T32UPV_20190904T102021\\bandTCIRGB\\Train\\redBand')
+        blue_images = self.open_files('T32UPV_20190904T102021\\bandTCIRGB\\Train\\blueBand')
+        greenBand = self.open_files('T32UPV_20190904T102021\\bandTCIRGB\\Train\\greenBand')
+        if include_nir:
+            nirBand = self.open_files('T32UPV_20190904T102021\\bandTCIRGB\\Train\\nirBand')
+        test = np.stack([np.array(Image.open(Path.joinpath(self.processed_path,'T32UPV_20190904T102021\\bandTCIRGB\\Train\\redBand\\_01_07.tiff'))),
+                            np.array(Image.open(Path.joinpath(self.processed_path,'T32UPV_20190904T102021\\bandTCIRGB\\Train\\blueBand\\_01_05.tiff'))),
+                            np.array(Image.open(Path.joinpath(self.processed_path,'T32UPV_20190904T102021\\bandTCIRGB\\Train\\greenBand\\_01_04.tiff')))],axis=2)
+        raw_rgb = np.stack([red_images,
+                            blue_images,
+                            greenBand],axis=2)
+        #normalize the data into 0–1 scale
+        raw_rgb =(raw_rgb / np.iinfo(raw_rgb.dtype).max)
+        # Format is: Samples, Channels, Width, Height
+        raw_rgb = np.swapaxes(raw_rgb, 1, 2)
+        raw_rgb = np.swapaxes(raw_rgb,2,3)
+
+        return raw_rgb
 
 
     # def Load(self):
--- src/prepData/RGBImage.py
+++ src/prepData/RGBImage.py
@@ -18,6 +18,16 @@ class prepRBGdata:
         self.processed_path = Path.joinpath(self.localdir,'data\\processed')
         self.number_tiles = config.number_tiles
 
+    def initTestAndValSplit(self,tuples):
+        tiles_list = list(tuples)
+        index_list = range(tiles_list.__len__())
+        random.shuffle(index_list)
+        train_index = index_list[:int((len(index_list) + 1) * .80)]  # Remaining 80% to training set
+        test_index = index_list[int((len(index_list) + 1) * .80):]  # Splits 20% data to test set
+        train_split,index = tiles_list[:int((len(tiles_list) + 1) * .80)]  # Remaining 80% to training set
+        test_split = tiles_list[int((len(tiles_list) + 1) * .80):]  # Splits 20% data to test set
+        return train_index, test_index
+
     def LoadRGBdata(self):
         ## implemented to look into all for folders located in Raw
         subfolders = [f.path for f in os.scandir(self.data_path) if f.is_dir()]
@@ -37,47 +47,60 @@ class prepRBGdata:
         bandTCI_img = Image.open(path_org_img + '_TCI_10m.jp2')  # TCI
         bandTCI_img_path = Path.joinpath(self.iterim_path, id + 'bandTCI.tiff')
         bandTCI_img.save(bandTCI_img_path)
-        path, tiles =self.sliceData(bandTCI_img_path, id, 'bandTCI')
+
+        path = self.createRGBdir(id,'bandTCI')
+
+        tiles =self.sliceData(bandTCI_img_path)
+        ## Only cal once!
+        self.initTestAndValSplit(tiles)
+        train, test = self.trainTestSplit(tiles)
+        self.storeTrainAndTest(path,test,train,'TCI')
+
+        band2_img  = Image.open(path_org_img+ '_B02_10m.jp2') # blue
+        band2_img_path =Path.joinpath(self.iterim_path,id+'band2.tiff')
+        band2_img.save(band2_img_path)
+
+        tiles = self.sliceData(band2_img_path)
         test, train = self.trainTestSplit(tiles)
-        self.storeTrainAndTest(path,test,train)
-        # band2_img  = Image.open(path_org_img+ '_B02_10m.jp2') # blue
-        # band2_img_path =Path.joinpath(self.iterim_path,id+'band2.tiff')
-        # band2_img.save(band2_img_path)
-        # self.sliceData(band2_img_path, self.processed_path,id,'band2')
+        self.storeTrainAndTest(path, test, train, 'blueBand')
         #
-        # band3_img = Image.open(path_org_img + '_B03_10m.jp2')  # green
-        # band3_img_path = Path.joinpath(self.iterim_path, id + 'band3.tiff')
-        # band3_img.save(band3_img_path)
-        # self.sliceData(band3_img_path, self.processed_path, id, "band3")
-        #
-        # band4_img = Image.open(path_org_img + '_B04_10m.jp2')  # red
-        # band4_img_path = Path.joinpath(self.iterim_path, id + 'band4.tiff')
-        # band4_img.save(band4_img_path)
-        # self.sliceData(band4_img_path, self.processed_path, id, "band4")
+        band3_img = Image.open(path_org_img + '_B03_10m.jp2')  # green
+        band3_img_path = Path.joinpath(self.iterim_path, id + 'band3.tiff')
+        band3_img.save(band3_img_path)
 
-        return bandTCI_img_path
+        tiles = self.sliceData(band3_img_path)
+        test, train = self.trainTestSplit(tiles)
+        self.storeTrainAndTest(path, test, train, 'greenBand')
 
-    def trainTestSplit(self, tuples):
-        tiles_list = list(tuples)
-        random.shuffle(tiles_list)
-        train_data = tiles_list[:int((len(tiles_list) + 1) * .80)]  # Remaining 80% to training set
-        test_data = tiles_list[int((len(tiles_list) + 1) * .80):]  # Splits 20% data to test set
-        train_data = tuple(train_data)
-        test_data = tuple(test_data)
+        band4_img = Image.open(path_org_img + '_B04_10m.jp2')  # red
+        band4_img_path = Path.joinpath(self.iterim_path, id + 'band4.tiff')
+        band4_img.save(band4_img_path)
+
+        tiles = self.sliceData(band4_img_path)
+        test, train = self.trainTestSplit(tiles)
+        self.storeTrainAndTest(path, test, train, 'redBand')
+
+        return bandTCI_img_path,band2_img_path,band3_img_path,band4_img_path
+
+    def trainTestSplit(self, train,test):
+        train_data = tuple(train)
+        test_data = tuple(test)
         return train_data, test_data
-
-    def sliceData(self,path_org_img,id,bandNumber):
-        tiles = image_slicer.slice(path_org_img, self.number_tiles, save=False)
+    def createRGBdir(self,id,bandNumber):
         localPath = Path.joinpath(self.processed_path,id)
         os.makedirs(localPath,exist_ok=True)
         path_store_img=Path.joinpath(localPath,bandNumber+'RGB')
         os.makedirs(path_store_img, exist_ok=True)
-        return path_store_img,tiles
+        return path_store_img
 
-    def storeTrainAndTest(self,path,test_data,train_data):
+    def sliceData(self,path_org_img):
+        tiles = image_slicer.slice(path_org_img, self.number_tiles, save=False)
+        return tiles
+
+    def storeTrainAndTest(self,path,test_data,train_data,folder_name):
 
         ## Train
-        train_path=Path.joinpath(path,"Train\\Train")
+        train_path=Path.joinpath(path,"Train\\"+folder_name)
 
         if os.path.exists(train_path):
             image_slicer.main.save_tiles(train_data, prefix='', directory=train_path, format='tiff')
@@ -97,10 +120,10 @@ class prepRBGdata:
         return
 
     def cleanUp(self,path_to_clean):
-        import os
-        if os.path.exists(path_to_clean):
-            os.remove(path_to_clean)
-        else:
-            print("The file does not exist")
+        for path in path_to_clean:
+            if os.path.exists(path_to_clean):
+                os.remove(path_to_clean)
+            else:
+                print("The file does not exist")
 
 
