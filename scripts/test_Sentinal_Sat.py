c16028071c7df1bc51244e702cd9bd5ca4881a5d
pandagud <pandagud@gmail.com>
2021-05-03 10:18:17 +0200

    local_test_script

---
+++ scripts/test_Sentinal_Sat.py
@@ -0,0 +1,131 @@
+import logging
+import click
+from src.config_default import TrainingConfig
+from src.config_utillity import update_config
+import os
+import geojson
+from geojson import Polygon,FeatureCollection,dump
+from pathlib import Path
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+from shapely.geometry import Polygon
+from shapely import geometry, affinity
+from shapely.ops import unary_union
+## TESSTING
+
+from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
+from datetime import date
+@click.command()
+@click.argument('args', nargs=-1)
+def main(args):
+    """ Runs dataLayer processing scripts to turn raw dataLayer from (../raw) into
+        cleaned dataLayer ready to be analyzed (saved in ../processed).
+    """
+    ## Talk to Rune about how dataLayer is handle. If it should be part of the "big" project.
+    ## set number_tiles:1764
+    config = TrainingConfig()
+    config = update_config(args,config)
+    logger = logging.getLogger(__name__)
+    logger.info('making final dataLayer set from raw dataLayer')
+    api = SentinelAPI('pandagud', 'damp4ever', 'https://scihub.copernicus.eu/dhus')
+
+    # search by polygon, time, and SciHub query keywords
+    path  = r"C:\Users\panda\Downloads\LC81820302014180LGN00.geojson"
+    footprint = geojson_to_wkt(read_geojson(path))
+    products = api.query(area=footprint, date=('20210401', '20210601'), producttype='GRD',
+                         platformname='Sentinel-1',sensoroperationalmode='IW',polarisationmode='VV VH')
+    ground_geojsons = read_geojson(path)
+    products_geojsons = api.to_geojson(products)
+
+    ground_polygon=ground_geojsons.get('features')[0].get('geometry').get('coordinates')
+    ground_polygon = geometry.Polygon(ground_polygon[0][0])
+    import numpy as np
+    titles = []
+    ids = []
+    for item in products_geojsons.get('features'):
+        id = item.get('properties').get('id')
+        item = item.get('properties').get('title')
+        item = (item[17:25] + item[48:55])
+        titles.append(item)
+        ids.append([item,id])
+    unique = list(set(titles))
+    union_list = []
+    for i,element in  enumerate(unique):
+        local_polygon = Polygon()
+        for j in range(len(titles)):
+            if titles[j]==element:
+                item = products_geojsons.get('features')[j]
+                item = item.get('geometry').get('coordinates')
+                item = geometry.Polygon(item[0][0])
+                item = affinity.scale(item,xfact=1.01,yfact=1.01)
+                polygons = [item,local_polygon]
+                local_polygons = unary_union(polygons)
+                local_polygon=item
+        union_list.append([local_polygons,element])
+    for index,element in enumerate(union_list):
+        wkt = element[0].wkt
+        if ground_polygon.within(element[0]):
+            found_id=element[1]
+            break
+    for i in ids:
+        if found_id!=i[0]:
+            del products[i[1]]
+    area_list=[]
+    for index,item in enumerate(products_geojsons.get('features')):
+        item=item.get('geometry').get('coordinates')
+        item = geometry.Polygon(item[0][0])
+        local_intersection = item.intersection(ground_polygon)
+        local_intersection = [local_intersection.area,index]
+        area_list.append(local_intersection)
+    area_list.sort(reverse=True)
+    for index in range(len(area_list)):
+        item = products_geojsons.get('features')[area_list[index][1]]
+        id = item.get('properties').get('id')
+        item=item.get('geometry').get('coordinates')
+        item = geometry.Polygon(item[0][0])
+        if item.intersects(ground_polygon):
+            local_intersection = ground_polygon.intersection(item)
+            print(str(ground_polygon.area))
+            print(str(local_intersection.area))
+            # ground_polygon = ground_polygon.difference(local_intersection)
+            ground_polygon = (ground_polygon.symmetric_difference(local_intersection)).difference(local_intersection)
+        else:
+            del products[id]
+    import datetime
+    from datetime import timedelta
+    S2_geojson = read_geojson(path)
+
+    start_S1_date = S2_geojson.get('features')[0].get('properties').get('ingestiondate')
+    start_S1_date = start_S1_date.split('T')[0]
+    start_S1_date = datetime.datetime.strptime(start_S1_date, '%Y-%m-%d').date()
+    ## New end date for S1
+    end_S1_date = start_S1_date + timedelta(days=7)
+    start_S1_date = start_S1_date -  timedelta(days=7)
+    start_S1_date_str=str(start_S1_date).replace('-','')
+    end_S1_date_str = str(end_S1_date).replace('-', '')
+
+
+
+
+
+        ## COMBINE FOOTPRINT
+    geom_in_geojson = []
+    geom_in_geojson.append(geojson.Feature(geometry=ground_polygon, properties={"MissingData":"Test"}))
+    feature_collection = FeatureCollection(geom_in_geojson)
+    pathToFile = r'C:\Users\panda\Sat_paper\missing.geojson'
+    with open(pathToFile, 'w') as f:
+        dump(feature_collection, f)
+
+
+    print("Done")
+
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    main()
